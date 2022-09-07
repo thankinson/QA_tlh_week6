2c776567ca2a323b719e910566355c860a9cb790
Tom <t_hankinson@outlook.com>
2022-09-07 16:30:32 +0100

    push

---
+++ flask_database/user.py
@@ -0,0 +1,10 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    dob = db.Column(db.DateTime(), nullable=False)
+    favourite_number = db.Column(db.Float(), nullable=True)
+    favourite_food = db.Column(db.String(3), default='MEX', nullable=False)
+
